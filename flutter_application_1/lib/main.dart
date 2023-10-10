ab970c08510de59bd2958d308ff3a6966b0c142e
Petar Dobrev <78638859+peterdobrev@users.noreply.github.com>
2023-10-10 13:27:18 +0300

    MARKING CLICKS WORKS (WITH NO IMAGE FORMATTING)
    Removed Image formatting widgets to ensure that the images are always placed at the top of the screen.

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -21,7 +21,6 @@ class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  // ignore: library_private_types_in_public_api
   _HomeScreenState createState() => _HomeScreenState();
 }
 
@@ -29,30 +28,45 @@ class _HomeScreenState extends State<HomeScreen> {
   Rect? highlightRect;
 
   final Rect differenceArea = Rect.fromPoints(
-    const Offset(0.1, 0.1),
-    const Offset(0.8, 0.9),
+    const Offset(0.0, 0.0),
+    const Offset(1.0, 1.0),
   );
 
   void onTap(TapUpDetails details, BuildContext context, bool isRightImage) {
     final RenderBox renderBox = context.findRenderObject() as RenderBox;
-    final size = renderBox.size;
+    final Offset offset =
+        renderBox.localToGlobal(Offset.zero); // Get the global position
 
+    // Calculate the local position
     final localPosition = renderBox.globalToLocal(details.globalPosition);
-    var dx = localPosition.dx / size.width;
-    final dy = localPosition.dy / size.height;
+
+    // Adjust the local position based on the widget's global position
+    final adjustedLocalPosition = localPosition - offset;
+
+    print('Global Offset: $offset');
+    print('Original Local Position: $localPosition');
+    print('Adjusted Local Position: $adjustedLocalPosition');
+
+    // Continue your existing logic, e.g., calculating relative coordinates
+    final size = renderBox.size;
+    var dx = adjustedLocalPosition.dx / size.width;
+    final dy = adjustedLocalPosition.dy / size.height;
 
     if (isRightImage) {
       dx -= 0.5;
     }
 
-    print('Tapped at: (${localPosition.dx};${localPosition.dy})');
+    print(
+        'Tapped at: (${adjustedLocalPosition.dx};${adjustedLocalPosition.dy})');
     print('Tapped at: ($dx;$dy)');
 
+    // Check if the tap is within the area of difference
     if (differenceArea.contains(Offset(dx, dy))) {
+      // Assuming highlightRect is a state variable
       setState(() {
         highlightRect = Rect.fromPoints(
-          Offset(localPosition.dx - 20, localPosition.dy - 20),
-          Offset(localPosition.dx + 20, localPosition.dy + 20),
+          Offset(adjustedLocalPosition.dx - 20, adjustedLocalPosition.dy - 20),
+          Offset(adjustedLocalPosition.dx + 20, adjustedLocalPosition.dy + 20),
         );
       });
     }
@@ -61,45 +75,40 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            buildImage(context, 'assets/vintage_car_in_woods_1.png', false),
-            buildImage(context, 'assets/vintage_car_in_woods_2.png', true),
-          ],
-        ),
+      body: Row(
+        children: [
+          buildImage(context, 'assets/vintage_car_in_woods_1.png', false),
+          buildImage(context, 'assets/vintage_car_in_woods_2.png', true),
+        ],
       ),
     );
   }
 
   Widget buildImage(BuildContext context, String imageName, bool isRightImage) {
-    return Expanded(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: GestureDetector(
-          onTapUp: (details) => onTap(details, context, isRightImage),
-          child: Stack(
-            children: [
-              Image.asset(imageName),
-              if (highlightRect != null)
-                Positioned(
-                  left: highlightRect!.left,
-                  top: highlightRect!.top,
-                  child: Container(
-                    width: highlightRect!.width,
-                    height: highlightRect!.height,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.red,
-                        width: 2,
-                      ),
-                    ),
+    return GestureDetector(
+      onTapUp: (details) => onTap(details, context, isRightImage),
+      child: Stack(
+        children: [
+          Image.asset(
+            imageName,
+            fit: BoxFit.cover, // This will cover the entire available space
+          ),
+          if (highlightRect != null)
+            Positioned(
+              left: highlightRect!.left,
+              top: highlightRect!.top,
+              child: Container(
+                width: highlightRect!.width,
+                height: highlightRect!.height,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.red,
+                    width: 2,
                   ),
                 ),
-            ],
-          ),
-        ),
+              ),
+            ),
+        ],
       ),
     );
   }
