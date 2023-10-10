c5579e190e6090e1e38805509af68d19fe38506b
Petar Dobrev <78638859+peterdobrev@users.noreply.github.com>
2023-10-10 15:13:48 +0300

    Converted to landscape
    Converted to landscape for mobile phone testing and use.

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -32,34 +32,28 @@ class _HomeScreenState extends State<HomeScreen> {
     const Offset(1.0, 1.0),
   );
 
-  void onTap(TapUpDetails details, BuildContext context, bool isRightImage) {
+  void onTap(TapUpDetails details, BuildContext context, bool isBottomImage) {
     final RenderBox renderBox = context.findRenderObject() as RenderBox;
-    final Offset offset =
-        renderBox.localToGlobal(Offset.zero); // Get the global position
-
-    // Calculate the local position
-    final localPosition = renderBox.globalToLocal(details.globalPosition);
-
-    // Adjust the local position based on the widget's global position
-    var adjustedLocalPosition = localPosition - offset;
-
+    final Offset localPosition =
+        renderBox.globalToLocal(details.globalPosition);
     final size = renderBox.size;
-    var dx = adjustedLocalPosition.dx / size.width;
-    final dy = adjustedLocalPosition.dy / size.height;
 
-    var x = adjustedLocalPosition.dx;
-    final y = adjustedLocalPosition.dy;
+    var dx = localPosition.dx / size.width;
+    var dy = localPosition.dy / size.height;
 
-    if (isRightImage) {
-      dx -= 0.5;
-      x -= size.width / 2;
+    var x = localPosition.dx;
+    var y = localPosition.dy;
+
+    if (isBottomImage) {
+      dy -= 0.5; // Offset if it is the bottom image
+      y -= size.height / 2;
     }
 
     if (differenceArea.contains(Offset(dx, dy))) {
       setState(() {
         highlightRect = Rect.fromPoints(
-          Offset(x - 20, y - 20),
-          Offset(x + 20, y + 20),
+          Offset(x - 50, y - 50),
+          Offset(x + 50, y + 50),
         );
       });
     }
@@ -68,7 +62,8 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
+      body: Column(
+        // Changed from Row to Column
         children: [
           buildImage(context, 'assets/vintage_car_in_woods_1.png', false),
           buildImage(context, 'assets/vintage_car_in_woods_2.png', true),
@@ -77,40 +72,33 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  Widget buildImage(BuildContext context, String imageName, bool isRightImage) {
+  Widget buildImage(
+      BuildContext context, String imageName, bool isBottomImage) {
     return Expanded(
       child: GestureDetector(
-        onTapUp: (details) => onTap(details, context, isRightImage),
-        child: LayoutBuilder(
-          builder: (BuildContext context, BoxConstraints constraints) {
-            // Now you have the constraints
-            print(
-                'Max Width: ${constraints.maxWidth}, Max Height: ${constraints.maxHeight}');
-            return Stack(
-              children: [
-                Image.asset(
-                  imageName,
-                  fit: BoxFit
-                      .cover, // This will cover the entire available space
-                ),
-                if (highlightRect != null)
-                  Positioned(
-                    left: highlightRect!.left,
-                    top: highlightRect!.top,
-                    child: Container(
-                      width: highlightRect!.width,
-                      height: highlightRect!.height,
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.red,
-                          width: 2,
-                        ),
-                      ),
+        onTapUp: (details) => onTap(details, context, isBottomImage),
+        child: Stack(
+          children: [
+            Image.asset(
+              imageName,
+              fit: BoxFit.cover,
+            ),
+            if (highlightRect != null)
+              Positioned(
+                left: highlightRect!.left,
+                top: highlightRect!.top,
+                child: Container(
+                  width: highlightRect!.width,
+                  height: highlightRect!.height,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.red,
+                      width: 2,
                     ),
                   ),
-              ],
-            );
-          },
+                ),
+              ),
+          ],
         ),
       ),
     );
