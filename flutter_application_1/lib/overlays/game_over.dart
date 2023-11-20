2ff99f27402b11336c712a8ba584cf4075990f90
Petar Dobrev <78638859+peterdobrev@users.noreply.github.com>
2023-11-21 00:35:21 +0200

    Improved animations

--- flutter_application_1/lib/overlays/game_over.dart
+++ flutter_application_1/lib/overlays/game_over.dart
@@ -2,61 +2,103 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/constants.dart';
 import 'package:flutter_application_1/differences_game.dart';
 
-class GameOver extends StatelessWidget {
-  // Reference to parent game.
+class GameOver extends StatefulWidget {
   final DifferencesGame game;
+
   const GameOver({super.key, required this.game});
+
+  @override
+  _GameOverState createState() => _GameOverState();
+}
+
+class _GameOverState extends State<GameOver>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 350),
+      vsync: this,
+    );
+
+    _animation = TweenSequence([
+      TweenSequenceItem(
+        tween: Tween<double>(begin: 0.0, end: 1.05)
+            .chain(CurveTween(curve: Curves.easeOut)),
+        weight: 50,
+      ),
+      TweenSequenceItem(
+        tween: Tween<double>(begin: 1.05, end: 1.0)
+            .chain(CurveTween(curve: Curves.easeIn)),
+        weight: 50,
+      ),
+    ]).animate(_controller);
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     const blackTextColor = Color.fromRGBO(0, 0, 0, 1.0);
     const whiteTextColor = Color.fromRGBO(255, 255, 255, 1.0);
 
-    return Material(
-      color: Colors.transparent,
-      child: Center(
-        child: Container(
-          padding: const EdgeInsets.all(10.0),
-          height: 200,
-          width: 300,
-          decoration: const BoxDecoration(
-            color: blackTextColor,
-            borderRadius: BorderRadius.all(
-              Radius.circular(20),
-            ),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Game Over',
-                style: TextStyle(
-                  color: whiteTextColor,
-                  fontSize: 24,
-                ),
+    return ScaleTransition(
+      scale: _animation,
+      child: Material(
+        color: Colors.transparent,
+        child: Center(
+          child: Container(
+            padding: const EdgeInsets.all(10.0),
+            height: 200,
+            width: 300,
+            decoration: const BoxDecoration(
+              color: blackTextColor,
+              borderRadius: BorderRadius.all(
+                Radius.circular(20),
               ),
-              const SizedBox(height: 40),
-              SizedBox(
-                width: 200,
-                height: 75,
-                child: ElevatedButton(
-                  onPressed: () {
-                    game.onLevelFail();
-                    game.overlays.remove(gameOverOverlayIdentifier);
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: whiteTextColor,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Game Over',
+                  style: TextStyle(
+                    color: whiteTextColor,
+                    fontSize: 24,
                   ),
-                  child: const Text(
-                    'Play Again',
-                    style: TextStyle(
-                      fontSize: 28.0,
-                      color: blackTextColor,
+                ),
+                const SizedBox(height: 40),
+                SizedBox(
+                  width: 200,
+                  height: 75,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      widget.game.resetLevel();
+                      widget.game.overlays.remove(gameOverOverlayIdentifier);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: whiteTextColor,
+                    ),
+                    child: const Text(
+                      'Play Again',
+                      style: TextStyle(
+                        fontSize: 28.0,
+                        color: blackTextColor,
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
--- flutter_application_1/lib/overlays/main_menu.dart
+++ flutter_application_1/lib/overlays/main_menu.dart
@@ -1,73 +1,104 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/differences_game.dart';
 
-class MainMenu extends StatelessWidget {
-  // Reference to parent game.
+class MainMenu extends StatefulWidget {
   final DifferencesGame game;
 
   const MainMenu({super.key, required this.game});
+
+  @override
+  _MainMenuState createState() => _MainMenuState();
+}
+
+class _MainMenuState extends State<MainMenu>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeOut,
+    );
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     const blackTextColor = Color.fromRGBO(0, 0, 0, 1.0);
     const whiteTextColor = Color.fromRGBO(255, 255, 255, 1.0);
 
-    return Material(
-      color: Colors.transparent,
-      child: Center(
-        child: Container(
-          padding: const EdgeInsets.all(10.0),
-          height: 250,
-          width: 300,
-          decoration: const BoxDecoration(
-            color: blackTextColor,
-            borderRadius: BorderRadius.all(
-              Radius.circular(20),
-            ),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Spot the Differences',
-                style: TextStyle(
-                  color: whiteTextColor,
-                  fontSize: 24,
+    return ScaleTransition(
+        scale: _animation,
+        child: Material(
+          color: Colors.transparent,
+          child: Center(
+            child: Container(
+              padding: const EdgeInsets.all(10.0),
+              height: 250,
+              width: 300,
+              decoration: const BoxDecoration(
+                color: blackTextColor,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20),
                 ),
               ),
-              const SizedBox(height: 40),
-              SizedBox(
-                width: 200,
-                height: 75,
-                child: ElevatedButton(
-                  onPressed: () {
-                    game.overlays.remove('MainMenu');
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: whiteTextColor,
-                  ),
-                  child: const Text(
-                    'Play',
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Spot the Differences',
                     style: TextStyle(
-                      fontSize: 40.0,
-                      color: blackTextColor,
+                      color: whiteTextColor,
+                      fontSize: 24,
                     ),
                   ),
-                ),
+                  const SizedBox(height: 40),
+                  SizedBox(
+                    width: 200,
+                    height: 75,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        widget.game.overlays.remove('MainMenu');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: whiteTextColor,
+                      ),
+                      child: const Text(
+                        'Play',
+                        style: TextStyle(
+                          fontSize: 40.0,
+                          color: blackTextColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const Text(
+                    '''Tap on the images where you see a difference!''',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: whiteTextColor,
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
               ),
-              const SizedBox(height: 20),
-              const Text(
-                '''Tap on the images where you see a difference!''',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: whiteTextColor,
-                  fontSize: 14,
-                ),
-              ),
-            ],
+            ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
